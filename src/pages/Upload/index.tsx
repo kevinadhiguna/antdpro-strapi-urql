b7d04f9b480c2d9d6ef3394e7db253f513dbc791
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-05-28 04:37:01 +0700

    refactor: add imports , useState, and useMutation

--- src/pages/Upload/index.tsx
+++ src/pages/Upload/index.tsx
@@ -1,4 +1,16 @@
+import { useState } from 'react';
+import { Form, Button } from 'antd';
+import { UploadOutlined } from '@ant-design/icons';
+
+import { useMutation } from 'urql';
+import { CREATEJUVENTUS, UPLOADPROFPIC } from '@/graphql/mutation';
+
 const Upload = () => {
+  const [profpic, setProfpic] = useState<File | null>(null);
+
+  const [CreateJuventus] = useMutation(CREATEJUVENTUS);
+  const [UploadProfpic] = useMutation(UPLOADPROFPIC);
+
   return (
     <>
       <h1>Upload page</h1>
