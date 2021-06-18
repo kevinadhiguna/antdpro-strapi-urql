e921cbc8a486c587e72b63abc45cfeda5e97b1fe
kevinadhiguna <hi.kevinadhiguna@gmail.com>
2021-06-18 08:36:59 +0000

    style: format code using Prettier

--- src/app.tsx
+++ src/app.tsx
@@ -15,9 +15,6 @@ import { multipartFetchExchange } from '@urql/exchange-multipart-fetch';
 
 import appConfig from './appConfig.json';
 
-import jwt_decode, { JwtPayload } from 'jwt-decode';
-import Swal from 'sweetalert2';
-
 const isDev = process.env.NODE_ENV === 'development';
 const loginPath = '/user/login';
 
