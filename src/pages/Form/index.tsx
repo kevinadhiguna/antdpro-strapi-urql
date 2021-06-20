62b74dc636b3cfe9e7d8dfaabed9c2f73cf382e0
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-06-20 15:43:37 +0700

    feat(alert): add an alert

--- src/pages/Form/index.tsx
+++ src/pages/Form/index.tsx
@@ -10,7 +10,7 @@ import ProForm, {
   ProFormDependency,
 } from '@ant-design/pro-form';
 import ProCard from '@ant-design/pro-card';
-import { Button, message } from 'antd';
+import { Alert, Button, message } from 'antd';
 
 const waitTime = (time: number = 100) => {
   return new Promise((resolve) => {
@@ -23,143 +23,153 @@ const waitTime = (time: number = 100) => {
 export default () => {
   const [loading, setLoading] = useState(false);
   return (
-    <ProCard>
-      <StepsForm
-        onFinish={async () => {
-          setLoading(true);
-          await waitTime(1000);
-          message.success('提交成功');
-          setLoading(false);
-        }}
-        submitter={{
-          render: ({ form, onSubmit, step, onPre }) => {
-            return [
-              <Button
-                key="rest"
-                onClick={() => {
-                  form?.resetFields();
-                }}
-              >
-                Reset
-              </Button>,
-              step > 0 && (
-                <Button
-                  key="pre"
-                  onClick={() => {
-                    onPre?.();
-                  }}
-                >
-                  Back
-                </Button>
-              ),
-              <Button
-                key="next"
-                loading={loading}
-                type="primary"
-                onClick={() => {
-                  onSubmit?.();
-                }}
-              >
-                Next
-              </Button>,
-            ];
-          },
-        }}
-        formProps={{
-          validateMessages: {
-            required: '此项为必填项',
-          },
-        }}
-      >
-        <StepsForm.StepForm
-          name="base"
-          title="创建实验"
+    <>
+      <Alert 
+        message="Warning"
+        description="This page is currently under the development..."
+        type="warning"
+        showIcon
+        closable
+        style={{ marginBottom: '8px' }}
+      />
+      <ProCard>
+        <StepsForm
           onFinish={async () => {
             setLoading(true);
-            await waitTime(2000);
+            await waitTime(1000);
+            message.success('提交成功');
             setLoading(false);
-            return true;
+          }}
+          submitter={{
+            render: ({ form, onSubmit, step, onPre }) => {
+              return [
+                <Button
+                  key="rest"
+                  onClick={() => {
+                    form?.resetFields();
+                  }}
+                >
+                  Reset
+                </Button>,
+                step > 0 && (
+                  <Button
+                    key="pre"
+                    onClick={() => {
+                      onPre?.();
+                    }}
+                  >
+                    Back
+                  </Button>
+                ),
+                <Button
+                  key="next"
+                  loading={loading}
+                  type="primary"
+                  onClick={() => {
+                    onSubmit?.();
+                  }}
+                >
+                  Next
+                </Button>,
+              ];
+            },
+          }}
+          formProps={{
+            validateMessages: {
+              required: '此项为必填项',
+            },
           }}
         >
-          <ProFormText
-            name="name"
-            label="实验名称"
-            width="md"
-            tooltip="最长为 24 位，用于标定的唯一 id"
-            placeholder="请输入名称"
-            rules={[{ required: true }]}
-          />
-          <ProFormDatePicker name="date" label="日期" />
-          <ProFormDateRangePicker name="dateTime" label="时间区间" />
-          <ProFormTextArea name="remark" label="备注" width="lg" placeholder="请输入备注" />
-        </StepsForm.StepForm>
-        <StepsForm.StepForm name="checkbox" title="设置参数">
-          <ProFormCheckbox.Group
-            name="checkbox"
-            label="迁移类型"
-            width="lg"
-            options={['结构迁移', '全量迁移', '增量迁移', '全量校验']}
-          />
-          <ProForm.Group>
-            <ProFormText name="dbName" label="业务 DB 用户名" />
-            <ProFormDatePicker name="datetime" label="记录保存时间" width="sm" />
-          </ProForm.Group>
-          <ProFormDependency name={['dbName']}>
-            {({ dbName }) => {
-              return (
-                <ProFormCheckbox.Group
-                  name="checkbox"
-                  label="迁移类型"
-                  options={dbName ? ['完整 LOB', '不同步 LOB', '受限制 LOB'] : ['完整 LOB']}
-                />
-              );
+          <StepsForm.StepForm
+            name="base"
+            title="创建实验"
+            onFinish={async () => {
+              setLoading(true);
+              await waitTime(2000);
+              setLoading(false);
+              return true;
             }}
-          </ProFormDependency>
-        </StepsForm.StepForm>
-        <StepsForm.StepForm name="time" title="发布实验">
-          <ProFormCheckbox.Group
-            name="checkbox"
-            label="部署单元"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-            options={['部署单元1', '部署单元2', '部署单元3']}
-          />
-          <ProFormSelect
-            label="部署分组策略"
-            name="remark"
-            rules={[
-              {
-                required: true,
-              },
-            ]}
-            initialValue="1"
-            width="md"
-            options={[
-              {
-                value: '1',
-                label: '策略一',
-              },
-              { value: '2', label: '策略二' },
-            ]}
-          />
-          <ProFormSelect
-            label="Pod 调度策略"
-            name="remark2"
-            initialValue="2"
-            width="md"
-            options={[
-              {
-                value: '1',
-                label: '策略一',
-              },
-              { value: '2', label: '策略二' },
-            ]}
-          />
-        </StepsForm.StepForm>
-      </StepsForm>
-    </ProCard>
+          >
+            <ProFormText
+              name="name"
+              label="实验名称"
+              width="md"
+              tooltip="最长为 24 位，用于标定的唯一 id"
+              placeholder="请输入名称"
+              rules={[{ required: true }]}
+            />
+            <ProFormDatePicker name="date" label="日期" />
+            <ProFormDateRangePicker name="dateTime" label="时间区间" />
+            <ProFormTextArea name="remark" label="备注" width="lg" placeholder="请输入备注" />
+          </StepsForm.StepForm>
+          <StepsForm.StepForm name="checkbox" title="设置参数">
+            <ProFormCheckbox.Group
+              name="checkbox"
+              label="迁移类型"
+              width="lg"
+              options={['结构迁移', '全量迁移', '增量迁移', '全量校验']}
+            />
+            <ProForm.Group>
+              <ProFormText name="dbName" label="业务 DB 用户名" />
+              <ProFormDatePicker name="datetime" label="记录保存时间" width="sm" />
+            </ProForm.Group>
+            <ProFormDependency name={['dbName']}>
+              {({ dbName }) => {
+                return (
+                  <ProFormCheckbox.Group
+                    name="checkbox"
+                    label="迁移类型"
+                    options={dbName ? ['完整 LOB', '不同步 LOB', '受限制 LOB'] : ['完整 LOB']}
+                  />
+                );
+              }}
+            </ProFormDependency>
+          </StepsForm.StepForm>
+          <StepsForm.StepForm name="time" title="发布实验">
+            <ProFormCheckbox.Group
+              name="checkbox"
+              label="部署单元"
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
+              options={['部署单元1', '部署单元2', '部署单元3']}
+            />
+            <ProFormSelect
+              label="部署分组策略"
+              name="remark"
+              rules={[
+                {
+                  required: true,
+                },
+              ]}
+              initialValue="1"
+              width="md"
+              options={[
+                {
+                  value: '1',
+                  label: '策略一',
+                },
+                { value: '2', label: '策略二' },
+              ]}
+            />
+            <ProFormSelect
+              label="Pod 调度策略"
+              name="remark2"
+              initialValue="2"
+              width="md"
+              options={[
+                {
+                  value: '1',
+                  label: '策略一',
+                },
+                { value: '2', label: '策略二' },
+              ]}
+            />
+          </StepsForm.StepForm>
+        </StepsForm>
+      </ProCard>
+    </>
   );
 };
