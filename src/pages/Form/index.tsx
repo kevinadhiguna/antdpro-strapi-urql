997bf12e0670fc5e840aec43a822786f2f23ddf8
kevinadhiguna <kevin17016@mail.unpad.ac.id>
2021-06-21 13:49:33 +0700

    feat(alert): add a development alert

--- src/pages/Form/index.tsx
+++ src/pages/Form/index.tsx
@@ -1,6 +1,8 @@
 import { UploadOutlined } from '@ant-design/icons';
 import { Form, Input, InputNumber, Button, Upload } from 'antd';
 
+import DevelopmentAlert from '@/components/DevelopmentAlert';
+
 const AddPlayerFormLayout = {
   labelCol: {
     span: 8,
@@ -40,133 +42,136 @@ const AddPlayer: React.FC = () => {
   };
 
   return (
-    <Form
-      {...AddPlayerFormLayout}
-      name="addPlayer"
-      onFinish={onFinish}
-      validateMessages={validateMessages}
-    >
-      <Form.Item
-        name="name"
-        label="Name"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
+    <>
+      <DevelopmentAlert />
+      <Form
+        {...AddPlayerFormLayout}
+        name="addPlayer"
+        onFinish={onFinish}
+        validateMessages={validateMessages}
       >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        name="number"
-        label="Number"
-        rules={[
-          {
-            required: true,
-            type: 'number',
-            min: 0,
-            max: 99,
-          },
-        ]}
-      >
-        <InputNumber />
-      </Form.Item>
-      <Form.Item
-        name="age"
-        label="Age"
-        rules={[
-          {
-            required: true,
-            type: 'number',
-            min: 0,
-            max: 99,
-          },
-        ]}
-      >
-        <InputNumber />
-      </Form.Item>
-      <Form.Item name="country" label="Country">
-        <Input />
-      </Form.Item>
-      <Form.Item
-        name="appearences"
-        label="Appearence(s)"
-        rules={[
-          {
-            required: true,
-            type: 'number',
-            min: 0,
-            max: 1000,
-          },
-        ]}
-      >
-        <InputNumber />
-      </Form.Item>
-      <Form.Item
-        name="goals"
-        label="Goal(s)"
-        rules={[
-          {
-            required: true,
-            type: 'number',
-            min: 0,
-          },
-        ]}
-      >
-        <InputNumber />
-      </Form.Item>
-      <Form.Item
-        name="minutesPlayed"
-        label="Minutes Played"
-        rules={[
-          {
-            required: true,
-            type: 'number',
-            min: 0,
-          },
-        ]}
-      >
-        <InputNumber />
-      </Form.Item>
-      <Form.Item
-        name="position"
-        label="Position"
-        rules={[
-          {
-            required: true,
-          },
-        ]}
-      >
-        <Input />
-      </Form.Item>
-      <Form.Item
-        name="profpic"
-        label="Profile Picture"
-        valuePropName="fileList"
-        getValueFromEvent={normFile}
-        extra="Please upload player's picture"
-        required={true}
-      >
-        <Upload
-          name="profilePicture"
-          listType="picture"
-          accept="image/png, image/jpeg"
-          maxCount={1}
-          // Use 'beforeUpload' function instead of 'onChange' function as 'onChange' function does not allow to upload a file
-          beforeUpload={(e) => {
-            return false;
-          }}
-          onRemove={() => {}}
+        <Form.Item
+          name="name"
+          label="Name"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
         >
-          <Button icon={<UploadOutlined />}>Click to Upload</Button>
-        </Upload>
-      </Form.Item>
-      <Form.Item wrapperCol={{ ...AddPlayerFormLayout.wrapperCol, offset: 8 }}>
-        <Button type="primary" htmlType="submit">
-          Submit
-        </Button>
-      </Form.Item>
-    </Form>
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="number"
+          label="Number"
+          rules={[
+            {
+              required: true,
+              type: 'number',
+              min: 0,
+              max: 99,
+            },
+          ]}
+        >
+          <InputNumber />
+        </Form.Item>
+        <Form.Item
+          name="age"
+          label="Age"
+          rules={[
+            {
+              required: true,
+              type: 'number',
+              min: 0,
+              max: 99,
+            },
+          ]}
+        >
+          <InputNumber />
+        </Form.Item>
+        <Form.Item name="country" label="Country">
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="appearences"
+          label="Appearence(s)"
+          rules={[
+            {
+              required: true,
+              type: 'number',
+              min: 0,
+              max: 1000,
+            },
+          ]}
+        >
+          <InputNumber />
+        </Form.Item>
+        <Form.Item
+          name="goals"
+          label="Goal(s)"
+          rules={[
+            {
+              required: true,
+              type: 'number',
+              min: 0,
+            },
+          ]}
+        >
+          <InputNumber />
+        </Form.Item>
+        <Form.Item
+          name="minutesPlayed"
+          label="Minutes Played"
+          rules={[
+            {
+              required: true,
+              type: 'number',
+              min: 0,
+            },
+          ]}
+        >
+          <InputNumber />
+        </Form.Item>
+        <Form.Item
+          name="position"
+          label="Position"
+          rules={[
+            {
+              required: true,
+            },
+          ]}
+        >
+          <Input />
+        </Form.Item>
+        <Form.Item
+          name="profpic"
+          label="Profile Picture"
+          valuePropName="fileList"
+          getValueFromEvent={normFile}
+          extra="Please upload player's picture"
+          required={true}
+        >
+          <Upload
+            name="profilePicture"
+            listType="picture"
+            accept="image/png, image/jpeg"
+            maxCount={1}
+            // Use 'beforeUpload' function instead of 'onChange' function as 'onChange' function does not allow to upload a file
+            beforeUpload={(e) => {
+              return false;
+            }}
+            onRemove={() => {}}
+          >
+            <Button icon={<UploadOutlined />}>Click to Upload</Button>
+          </Upload>
+        </Form.Item>
+        <Form.Item wrapperCol={{ ...AddPlayerFormLayout.wrapperCol, offset: 8 }}>
+          <Button type="primary" htmlType="submit">
+            Submit
+          </Button>
+        </Form.Item>
+      </Form>
+    </>
   );
 };
 
